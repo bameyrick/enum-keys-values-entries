8a33047d814f5d8031116c21aab1f2fc9356f9a9
Ben Meyrick <bameyrick@gmail.com>
2021-12-21 17:11:39 +0000

    Fix return types

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,13 @@
-type EnumType<E> = Record<keyof E, number | string> & { [k: string]: keyof E };
-
-function enumKeys<E>(_enum: E): string[] {
+function enumKeys<E>(_enum: { [k: string]: E }): string[] {
   return Object.keys(_enum).filter(key => isNaN(Number(key)));
 }
 
-function enumValues<E>(_enum: E): E[] {
-  return enumKeys(_enum).map(key => (_enum as unknown as EnumType<E>)[key] as unknown as E);
+function enumValues<E>(_enum: { [k: string]: E }): E[] {
+  return enumKeys(_enum).map(key => _enum[key]);
 }
 
-function enumEntries<E>(_enum: E): Array<[string, E]> {
-  return enumKeys(_enum).map(key => [key, (_enum as unknown as EnumType<E>)[key] as unknown as E]);
+function enumEntries<E>(_enum: { [k: string]: E }): Array<[string, E]> {
+  return enumKeys(_enum).map(key => [key, _enum[key]]);
 }
 
 export const Enum = {
