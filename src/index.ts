3a6f542e45a571eb8d2bd7ef06f1a4a99807ab81
Ben Meyrick <bameyrick@gmail.com>
2021-12-21 17:22:52 +0000

    Fix return types

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-function enumKeys<E>(_enum: { [k: string]: E }): string[] {
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+function enumKeys<E>(_enum: { [k: string]: E } | any): string[] {
   return Object.keys(_enum).filter(key => isNaN(Number(key)));
 }
 
-function enumValues<E>(_enum: { [k: string]: E }): E[] {
+function enumValues<E>(_enum: { [k: string]: E } | any): E[] {
   return enumKeys(_enum).map(key => _enum[key]);
 }
 
-function enumEntries<E>(_enum: { [k: string]: E }): Array<[string, E]> {
+function enumEntries<E>(_enum: { [k: string]: E } | any): Array<[string, E]> {
   return enumKeys(_enum).map(key => [key, _enum[key]]);
 }
 
