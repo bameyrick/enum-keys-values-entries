2c86a5f7bc9b17e54f3260525b176bdca8a1d406
Ben Meyrick <bameyrick@gmail.com>
2024-01-02 15:12:34 +0000

    Fix typisation for Values/Keys/Entries (#4)
    * Fix typisation
    
    * Change to use github workflows for tests
    
    * Change to use test-single command
    
    * Add publish workflow

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-function enumKeys<E>(_enum: { [k: string]: E } | any): string[] {
-  return Object.keys(_enum).filter(key => isNaN(Number(key)));
+function enumKeys<E extends { [k: string]: any }>(_enum: E): Array<keyof E> {
+  return Object.keys(_enum).filter(key => isNaN(Number(key))) as Array<keyof E>;
 }
 
-function enumValues<E>(_enum: { [k: string]: E } | any): E[] {
-  return enumKeys(_enum).map(key => _enum[key]);
+function enumValues<E extends { [k: string]: any }>(_enum: E): E[] {
+  return enumKeys(_enum).map(key => _enum[key]) as E[];
 }
 
-function enumEntries<E>(_enum: { [k: string]: E } | any): Array<[string, E]> {
-  return enumKeys(_enum).map(key => [key, _enum[key]]);
+function enumEntries<E extends { [k: string]: any }>(_enum: E): Array<[keyof E, E]> {
+  return enumKeys(_enum).map(key => [key, _enum[key]]) as Array<[keyof E, E]>;
 }
 
 export const Enum = {
