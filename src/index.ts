1987a4fe65498da6adcaf0b6941693188142d3d5
Ben Meyrick <bameyrick@gmail.com>
2024-01-10 12:55:37 +0000

    Fix typisation for enums functions

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-function enumKeys<E extends { [k: string]: any }>(_enum: E): Array<keyof E> {
-  return Object.keys(_enum).filter(key => isNaN(Number(key))) as Array<keyof E>;
+function enumKeys<E, K extends string>(_enum: { [key in K]: E }): K[] {
+  return Object.keys(_enum).filter(key => isNaN(Number(key))) as K[];
 }
 
-function enumValues<E extends { [k: string]: any }>(_enum: E): E[] {
+function enumValues<E, K extends string>(_enum: { [key in K]: E }): E[] {
   return enumKeys(_enum).map(key => _enum[key]) as E[];
 }
 
-function enumEntries<E extends { [k: string]: any }>(_enum: E): Array<[keyof E, E]> {
-  return enumKeys(_enum).map(key => [key, _enum[key]]) as Array<[keyof E, E]>;
+function enumEntries<E, K extends string>(_enum: { [key in K]: E }): [K, E][] {
+  return enumKeys(_enum).map(key => [key, _enum[key]] as [K, E]);
 }
 
 export const Enum = {
